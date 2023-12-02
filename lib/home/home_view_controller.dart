589e84aed67cf9123a1bf51f7151aaaaa5c5adfa
rodrigogaleano <rodrigogaleano@jera.com.br>
2023-12-02 17:30:41 -0400

    [NEW] Add go router

---
+++ lib/home/home_view_controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeViewController extends StatefulWidget {
+  const HomeViewController({super.key});
+
+  @override
+  State<HomeViewController> createState() => _HomeViewControllerState();
+}
+
+class _HomeViewControllerState extends State<HomeViewController> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/router/mobile_router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:movies/home/home_view_controller.dart';
+
+class MobileRouter {
+  static final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
+
+  static const String homeRoute = '/home';
+
+  static final GoRouter router = GoRouter(
+    navigatorKey: rootNavigatorKey,
+    initialLocation: homeRoute,
+    routes: <RouteBase>[
+      GoRoute(
+        path: homeRoute,
+        name: homeRoute,
+        builder: (_, __) => const HomeViewController(),
+      ),
+    ],
+  );
+}
